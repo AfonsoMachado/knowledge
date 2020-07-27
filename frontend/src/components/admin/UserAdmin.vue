f8c794a5403730ee809e65001650a8dd08b2715e
Afonso Machado <afonso777@hotmail.com>
2020-07-26 23:41:14 -0300

    abas na parte de adm, criação dos componentes adm

---
+++ frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="user-admin">
+    <h1>usuario componente</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserAdmin",
+};
+</script>
+
+<style>
+</style>
