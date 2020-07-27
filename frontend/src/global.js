401f9f5cb8af3f8b9ddda1ff648f1ebdf3e81924
Afonso Machado <afonso777@hotmail.com>
2020-07-27 18:45:55 -0300

    implementando mensagem toas de sucesso e erro

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,2 +1,19 @@
+import Vue from 'vue';
+
 // Apontando para o backend
 export const baseApiUrl = 'http://localhost:3000'
+
+// Função para tratar mensagem de error
+export function showError(e) {
+  // Testando erros em cenarios comuns
+  if (e && e.response && e.response.data) {
+    Vue.toasted.global.defaultError({ msg: e.response.data })
+    // Caso o erro seja uma string
+  } else if (typeof e === 'string') {
+    Vue.toasted.global.defaultError({ msg: e })
+  } else {
+    Vue.toasted.global.defaultError()
+  }
+}
+
+// export default { baseApiUrl, showError }
