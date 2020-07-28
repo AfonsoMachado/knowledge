495a20cf86bee1ff5491b5daa9407883d9a7abb3
Afonso Machado <afonso777@hotmail.com>
2020-07-28 19:51:49 -0300

    alterando store para adição de login de usuario

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -1,23 +1,37 @@
 // área de armazenamento, para compartilhar entre os componentes
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isMenuVisible: true,
-    user: {
-      name: 'Usuário Mock',
-      email: 'mock@gmail.com'
-    }
+    isMenuVisible: false,
+    user: null
   },
   mutations: {
     toggleMenu(state, isVisible) {
+      // Evitar que o menu apareça quando o usuario n estiver setado
+      if (!state.user) {
+        state.isMenuVisible = false
+        return
+      }
+
       if (isVisible === undefined) {
         state.isMenuVisible = !state.isMenuVisible
       } else {
         state.isMenuVisible = isVisible
+      }
+    },
+    setUser(state, user) {
+      state.user = user
+      if (user) {
+        axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
+        state.isMenuVisible = true
+      } else {
+        delete axios.defaults.headers.common['Authorization']
+        state.isMenuVisible = false
       }
     }
   },
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMP => Token para qualquer requisição que o axios fizer, para o mesmo ir encorporado, no header da requisição
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFmb25zbyBkYSBTaWx2YSBNYWNoYWRvIiwiZW1haWwiOiJhZm9uc283NzdAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTk1ODgzMDAzLCJleHAiOjE1OTYxNDIyMDN9.58gx7sSe3No2Ng6MK1nmtTczQ8AHl0GuNCNyc8mnClg';
+// require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFmb25zbyBkYSBTaWx2YSBNYWNoYWRvIiwiZW1haWwiOiJhZm9uc283NzdAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTk1ODgzMDAzLCJleHAiOjE1OTYxNDIyMDN9.58gx7sSe3No2Ng6MK1nmtTczQ8AHl0GuNCNyc8mnClg';
 
 new Vue({
   store,
