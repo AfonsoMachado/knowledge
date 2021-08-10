b81a2d71b6de951ac77a0b8bf9e9f581f6dc9065
Afonso Machado <afonso777@hotmail.com>
2021-08-09 23:25:24 -0300

    rotas?

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const success = res => res
+const success = (res) => res;
 
-const error = err => {
+const error = (err) => {
   if (401 === err.response.status) {
     // recarrega a pagina para verificar novamente o token
     // caso esteja expeirado, vai para a tela de login
-    window.location = '/'
+    window.location = '/';
   } else {
-    return Promise.reject(err)
+    return Promise.reject(err);
   }
-}
+};
 
-axios.interceptors.response.use(success, error)
+axios.interceptors.response.use(success, error);
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,17 @@
-import 'font-awesome/css/font-awesome.css'
-import Vue from 'vue'
+import 'font-awesome/css/font-awesome.css';
+import Vue from 'vue';
 
-import App from './App'
+import App from './App';
 
-import './config/bootstrap'
-import './config/msgs'
-import './config/axios'
-import './config/mq'
+import './config/bootstrap';
+import './config/msgs';
+import './config/axios';
+import './config/mq';
 
 import store from './config/store';
-import router from './config/router'
+import router from './config/router';
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // TEMP => Token para qualquer requisição que o axios fizer, para o mesmo ir encorporado, no header da requisição
 // require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFmb25zbyBkYSBTaWx2YSBNYWNoYWRvIiwiZW1haWwiOiJhZm9uc283NzdAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTk1ODgzMDAzLCJleHAiOjE1OTYxNDIyMDN9.58gx7sSe3No2Ng6MK1nmtTczQ8AHl0GuNCNyc8mnClg';
@@ -20,5 +19,5 @@ Vue.config.productionTip = false
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
